ac8024a213e3e3b016fc2267a62423139a7e3437
mayank0-2 <mayank02091999@gmail.com>
2023-06-06 12:32:01 +0530

    home ui template in place

--- project/notesapp/home/views.py
+++ project/notesapp/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from newnotes.models import FormModel
 # Create your views here.
 
+
 def home_view(request) :
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated :
+        data = FormModel.objects.all()
+    else :
+        data = None
+    return render(request, 'home.html', {'data': data})
