3b10d1f05be5eaa5e64b82dd43d8b8a9834a49e2
mayank0-2 <mayank02091999@gmail.com>
2023-06-06 08:38:19 +0530

    updated duplicate storage issue

--- project/notesapp/newnotes/views.py
+++ project/notesapp/newnotes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from newnotes.forms import DisplayForm
 from django.shortcuts import redirect
+from newnotes.models import FormModel
 # Create your views here.
 
 @login_required
@@ -9,9 +10,14 @@ def NewNotes(request) :
     if request.method == 'POST' :
         form = DisplayForm(request.POST)
         if form.is_valid() :
-            form.save()
-            # return render(request, 'home.html', {})
-            return redirect ('/')
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            data = FormModel.objects.filter(title=title, description=description).exists()
+            if data :
+                return redirect ('/')
+            else :
+                form.save()
+                return redirect ('/')
             
     else :
         form = DisplayForm()
